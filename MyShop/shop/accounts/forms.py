c4795176374d581d920bdb6f8b448ff8f718ce67
olegveshchykov <ipz222_vom@student.ztu.edu.ua>
2025-08-09 21:36:22 +0300

    Add user authentication: login & registration views and templates

---
+++ MyShop/shop/accounts/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Введіть email'
+    }))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Ім’я користувача'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Пароль'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Підтвердження пароля'})
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Ім’я користувача'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Пароль'})
--- MyShop/shop/main/models.py
+++ MyShop/shop/main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=200, verbose_name="Назва товару")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Ціна")
-    image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name="Фото")
+    image = models.ImageField(upload_to='main/img/products/', blank=True, null=True, verbose_name="Фото")
     description = models.TextField(verbose_name="Опис")
 
     def __str__(self):
